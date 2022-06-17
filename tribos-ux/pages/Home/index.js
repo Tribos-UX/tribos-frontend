d35cf68cdd9560091a810733660dd9ce6d6b4e7e
Viniciusmbc <viniciuscaceres@gmail.com>
2022-06-17 17:24:23 -0300

    feature: add navbar

--- tribos-ux/pages/Home/index.js
+++
@@ -1,9 +0,0 @@
-function index() {
-  return (
-    <div>
-      <h1>Hello World! </h1>
-    </div>
-  );
-}
-
-export default index;
---
+++ tribos-ux/pages/login.js
@@ -0,0 +1,10 @@
+//Components
+import Header from "../components/Header";
+
+export default function Login() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
