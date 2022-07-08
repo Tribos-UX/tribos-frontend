f8864b196a2b0652fef68d9856b791073c79c4d3
Viniciusmbc <viniciuscaceres@gmail.com>
2022-07-07 21:40:50 -0300

    feat/stylebutton

--- tribos-ux/pages/signup.js
+++ tribos-ux/pages/signup.js
@@ -1,9 +1,13 @@
+// Components
 import { Button } from "../components/Button";
 
-// import styles from modules
+// Icons
+import { ExclamationMark, exclamationMark } from "../components/Icons";
+
+// Styles modules
 import styles from "../styles/Signup.module.scss";
 
-//import images from public
+// Images
 import loginImg from "../public/login.jpg";
 
 //Nextjs tools
@@ -12,38 +16,6 @@ import Link from "next/link";
 
 // Layout for the page
 import NestedLayout from "../components/NestedLayout";
-
-const fbIcon = (
-  <svg
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg">
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M0 12C0 5.37258 5.37258 0 12 0C18.6274 0 24 5.37258 24 12C24 18.6274 18.6274 24 12 24C5.37258 24 0 18.6274 0 12ZM13.2508 19.0557V12.5271H15.053L15.2918 10.2773H13.2508L13.2538 9.15125C13.2538 8.56447 13.3096 8.25006 14.1524 8.25006H15.279V6H13.4766C11.3116 6 10.5495 7.0914 10.5495 8.92678V10.2776H9.2V12.5274H10.5495V19.0557H13.2508Z"
-      fill="#2D5BFF"
-    />
-  </svg>
-);
-
-const googleIcon = (
-  <svg
-    width="24"
-    height="24"
-    viewBox="0 0 16 16"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg">
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M8 0C3.58133 0 0 3.582 0 8C0 12.418 3.58133 16 8 16C12.418 16 16 12.418 16 8C16 3.582 12.418 0 8 0ZM8.09333 12.6787C5.51467 12.6787 3.42667 10.5853 3.42667 8C3.42667 5.41467 5.51467 3.32133 8.09333 3.32133C9.35333 3.32133 10.4067 3.786 11.2147 4.54067L9.89867 5.85933V5.85667C9.40867 5.38867 8.78733 5.14867 8.09333 5.14867C6.55333 5.14867 5.302 6.45267 5.302 7.99733C5.302 9.54067 6.55333 10.8487 8.09333 10.8487C9.49067 10.8487 10.4413 10.0473 10.6373 8.94733H8.09333V7.12267H12.4833C12.542 7.436 12.5733 7.76267 12.5733 8.10533C12.5733 10.7787 10.7887 12.6787 8.09333 12.6787Z"
-      fill="#D87036"
-    />
-  </svg>
-);
 
 export default function Signup() {
   return (
@@ -71,36 +43,13 @@ export default function Signup() {
             <input placeholder="Digite sua senha" type="password" />
           </fieldset>
 
-          <p>
-            <span>
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M8.00004 14.6668C11.6667 14.6668 14.6667 11.6668 14.6667 8.00016C14.6667 4.3335 11.6667 1.3335 8.00004 1.3335C4.33337 1.3335 1.33337 4.3335 1.33337 8.00016C1.33337 11.6668 4.33337 14.6668 8.00004 14.6668Z"
-                  stroke="#797C80"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M8 5.3335V8.66683"
-                  stroke="#797C80"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M7.99634 10.6665H8.00233"
-                  stroke="#797C80"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </span>
-            Mínimo de 6 caracteres, pelo menos um número e um caractere especial
-          </p>
+          <div className={styles.password_advisor}>
+            <ExclamationMark />
+            <p>
+              Mínimo de 6 caracteres, pelo menos um número e um caractere
+              especial
+            </p>
+          </div>
 
           <fieldset className={styles.password_input}>
             <legend>Senha</legend>
@@ -134,8 +83,8 @@ export default function Signup() {
         </div>
 
         <div className={styles.buttons}>
-          <Button text={"Google"} svg={googleIcon} />
-          <Button text={"Facebook"} svg={fbIcon} />
+          <Button text={"Google"} svg={"google"} />
+          <Button text={"Facebook"} svg={"facebook"} />
         </div>
 
         <div className={styles.info_signup}>
