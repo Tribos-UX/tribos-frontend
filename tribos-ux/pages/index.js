bffb33b7b0a2c28ac270efa6cbf24c2da0a35cf1
Viniciusmbc <viniciuscaceres@gmail.com>
2022-07-09 10:31:10 -0300

    feat/home

--- tribos-ux/pages/index.js
+++ tribos-ux/pages/index.js
@@ -1,8 +1,21 @@
 // Nextjs tools
 import Head from "next/head";
+import Image from "next/image";
 
 // Components
 import Layout from "../components/Layouts";
+import { Button } from "../components/Button";
+
+//Images
+import loginImg from "../public/login.jpg";
+import FotoPessoa from "../public/fotoPessoaHome.png";
+
+//Icons
+import { Sublinhado } from "../components/Icons";
+
+// Styles
+import styles from "../styles/Home.module.scss";
+import UxTribosTitle from "../components/UxTribosTitle";
 
 export default function Home() {
   return (
@@ -13,7 +26,38 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <section></section>
+      <section className={styles.principal}>
+        <article className={styles.descricao}>
+          <h1>
+            O
+            <UxTribosTitle />
+            te ajuda a encontrar o grupo de estudos ideal
+          </h1>
+          <p>
+            Somos uma comunidade de pessoas com um propósito em comum — a troca
+            de experiência. Aqui você encontra grupos de acordo com sua
+            localidade, interesses e objetivos em comum. Vamos lá?
+          </p>
+          <Button text={`Quero conhecer`} />
+        </article>
+        <Image
+          src={FotoPessoa}
+          alt="Foto de uma pessoa"
+          width={459}
+          height={724}
+        />
+      </section>
+      <section>
+        <article>
+          <p>
+            Utilize as mesmas plataformas que os melhores profissionais do
+            mercado.
+          </p>
+        </article>
+        <article>
+          <Image src={loginImg} alt="Image Login" />
+        </article>
+      </section>
     </main>
   );
 }
