7c9fd5208ba0262126ee56e88f5b734e89f4b7b0
liafer <106263459+liafer@users.noreply.github.com>
2022-11-02 18:43:28 -0400

    updates

---
+++ tribos-ux/components/Agenda.js
@@ -0,0 +1,81 @@
+// Icons
+import { agendaIcon, flecha, pinIcon, plusIconBorder, setaIcon } from "./Icons";
+
+// Better Carousel
+import Carousel from "better-react-carousel";
+
+// Styles
+import styles from "./Agenda.module.scss";
+import DaysOfweek from "./DaysOfweek";
+
+export default function Agenda({ dayOfweek }) {
+    return (
+        <div className={styles.container}>
+            <div className={styles.titulo}>
+                <span className={styles.agenda_icon}>{agendaIcon}</span>
+                Sua agenda
+                <span className={styles.plus_icon}>{plusIconBorder}</span>
+            </div>
+            <div>
+                <Carousel
+                    rows={1}
+                    cols={6}
+                    containerStyle={{
+                        background: "#FBFBFC",
+                        "box-shadow": "0px 4px 8px -2px rgba(16, 24, 40, 0.1)",
+                        "border-radius": "16px",
+                    }}>
+                    <Carousel.Item>
+                        <DaysOfweek day={"seg"} number={"01"} />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <DaysOfweek day={"seg"} number={"01"} />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <DaysOfweek day={"ter"} number={"02"} />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <DaysOfweek day={"qua"} number={"03"} />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <DaysOfweek day={"qui"} number={"04"} />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <DaysOfweek day={"sex"} number={"05"} />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <DaysOfweek day={"sab"} number={"06"} />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <DaysOfweek day={"sab"} number={"06"} />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <DaysOfweek day={"sab"} number={"06"} />
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+            <div className={styles.dayOfweek}>{dayOfweek}</div>
+            <div className={styles.tarefas}>
+                <p>
+                    <span>{pinIcon}</span>Pesquisa qualitativa
+                </p>
+                {setaIcon}
+            </div>
+            <div className={styles.tarefas}>
+                {pinIcon}
+                <p>Pesquisa qualitativa</p>
+                {setaIcon}
+            </div>
+            <div className={styles.tarefas}>
+                {pinIcon}
+                <p>Pesquisa qualitativa</p>
+                {setaIcon}
+            </div>
+            <div className={styles.tarefas}>
+                {pinIcon}
+                <p>Pesquisa qualitativa</p>
+                {setaIcon}
+            </div>
+        </div>
+    );
+}
--- tribos-ux/pages/dashboard/groups.js
+++ tribos-ux/pages/dashboard/groups.js
@@ -1,3 +1,77 @@
+// Nextjs Image
+import Image from "next/future/image";
+import Button from "../../components/Button";
+
+// Dashboard Layout
+import DashboardLayout from "../../components/DashboardLayout";
+import GroupCards from "../../components/GroupCards";
+import { shareIcon, sinalMais } from "../../components/Icons";
+import Agenda from "../../components/Agenda";
+
+// Images
+import groupsImageRectangle from "../../public/groupsImageRectangle.png";
+import imagemPerfilGroups from "../../public/imagemPerfilGroups.png";
+
+// Styles
+import styles from "../../styles/Groups.module.scss";
+
 export default function Groups() {
-  return <div>groups</div>;
+  return (
+    <>
+      <section>
+        <section className={styles.groups_usuario}>
+          <div className={styles.container}>
+            <div className={styles.groups_usuario_imagem_principal}>
+              <Image
+                className={styles.groups_usuario_imagem_rectangle}
+                src={groupsImageRectangle}
+                alt="Imagem tema do usuario"
+              />
+              <Image
+                src={imagemPerfilGroups}
+                alt="Imagem de Perfil"
+                className={styles.groups_usuario_imagem_perfil}
+              />
+            </div>
+            <div className={styles.groups_usuario_infos}>
+              <div className={styles.groups_usuario_infos_descricao}>
+                <h1>Felipe Soares</h1>
+                <p>UX Designer</p>
+              </div>
+              <div className={styles.groups_usuario_infos_buttons}>
+                <Button text={"Research"} />
+                <Button text={"Wireframe"} />
+                <Button text={"Agile"} />
+                {shareIcon}
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className={`${styles.container}`}>
+            <nav className={styles.grupos_usario}>
+              <ul>
+                <li>Meus grupos</li>
+                <li>Grupos que administro</li>
+              </ul>
+            </nav>
+            <article className={styles.grupos_container}>
+              <button className={styles.grupos_usario_button}>
+                <div className={styles.grupos_usuario_signal}>{sinalMais}</div>
+                <div className={styles.grupos_usuario_text}>Criar Grupo</div>
+              </button>
+              <GroupCards />
+            </article>
+          </div>
+        </section>
+      </section>
+      <section>
+        <Agenda dayOfweek={"Segunda-feira, 01 de julho de 2022"} />
+      </section>
+    </>
+  );
 }
+
+Groups.getLayout = function getLayout(page) {
+  return <DashboardLayout>{page}</DashboardLayout>;
+};
