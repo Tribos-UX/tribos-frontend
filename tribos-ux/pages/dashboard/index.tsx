755d09f83c3a2710f6e1e6e86c9982bbb0db6657
viniciusmbc <viniciusmbc>
2023-01-17 18:00:09 -0300

    fix: dashboard styles

--- tribos-ux/pages/dashboard/index.tsx
+++ tribos-ux/pages/dashboard/index.tsx
@@ -35,72 +35,74 @@ export default function Welcome({ user }) {
   }
 
   return (
-    <section className={styles.dashboard_welcome}>
-      <section className={styles.container}>
-        <h1
-          className={
-            formStep === 2
-              ? styles.dashboard_titulo_hidden
-              : styles.dashboard_titulo
-          }
-        >
-          Olá, seja bem-vindo(a)! <br></br>
-          Vamos configurar seu
-          <strong className={styles.dashboard_palavra_sublinhada}>
-            perfil?
-            <span>
-              <SublinhadoMenor />
-            </span>
-          </strong>
-        </h1>
+    <>
+      <section className={styles.dashboard_welcome}>
+        <section className={styles.container}>
+          <h1
+            className={
+              formStep === 2
+                ? styles.dashboard_titulo_hidden
+                : styles.dashboard_titulo
+            }
+          >
+            Olá, seja bem-vindo(a)! <br></br>
+            Vamos configurar seu
+            <strong className={styles.dashboard_palavra_sublinhada}>
+              perfil?
+              <span>
+                <SublinhadoMenor />
+              </span>
+            </strong>
+          </h1>
 
-        <div className={styles.dashboard_steps_indicator}>
-          <figure className={formStep === 0 ? styles.step_now : null}>
-            {formStep === 0 ? (
-              stepIndicatorNumber1
-            ) : (
-              <Image src={step_ok} alt={'Está ok'} width={42} height={42} />
-            )}
-          </figure>
-          <span className={styles.linha}></span>
-          <figure className={formStep === 1 ? styles.step_now : null}>
-            {formStep >= 2 ? (
-              <Image src={step_ok} alt={'Está ok'} width={42} height={42} />
-            ) : (
-              stepIndicatorNumber2
-            )}
-          </figure>
-          <span className={styles.linha}></span>
-          <figure>
-            {formStep >= 2 ? (
-              <Image src={step_ok} alt={'Está ok'} width={42} height={42} />
-            ) : (
-              stepIndicatorNumber3
-            )}
-          </figure>
-        </div>
+          <div className={styles.dashboard_steps_indicator}>
+            <figure className={formStep === 0 ? styles.step_now : null}>
+              {formStep === 0 ? (
+                stepIndicatorNumber1
+              ) : (
+                <Image src={step_ok} alt={'Está ok'} width={42} height={42} />
+              )}
+            </figure>
+            <span className={styles.linha}></span>
+            <figure className={formStep === 1 ? styles.step_now : null}>
+              {formStep >= 2 ? (
+                <Image src={step_ok} alt={'Está ok'} width={42} height={42} />
+              ) : (
+                stepIndicatorNumber2
+              )}
+            </figure>
+            <span className={styles.linha}></span>
+            <figure>
+              {formStep >= 2 ? (
+                <Image src={step_ok} alt={'Está ok'} width={42} height={42} />
+              ) : (
+                stepIndicatorNumber3
+              )}
+            </figure>
+          </div>
 
-        {formStep === 3 ? (
-          <h2 className={styles.dashboard_subtitulo}>
-            Gostariamos de saber um pouco mais sobre você{' '}
-          </h2>
-        ) : null}
+          {formStep === 3 ? (
+            <h2 className={styles.dashboard_subtitulo}>
+              Gostariamos de saber um pouco mais sobre você{' '}
+            </h2>
+          ) : null}
 
-        {formStep === 0 && (
-          <>
-            <CadastroForm1 nextForm={nextForm} id={user.id} />
-          </>
-        )}
-        {formStep === 1 && (
-          <>
-            <CadastroForm2 id={user.id} nextForm={nextForm} />
-          </>
-        )}
-        {formStep === 2 && (
-          <>
-            <CadastroEnd />
-          </>
-        )}
+          {formStep === 0 && (
+            <>
+              <CadastroForm1 nextForm={nextForm} id={user.id} />
+            </>
+          )}
+          {formStep === 1 && (
+            <>
+              <CadastroForm2 id={user.id} nextForm={nextForm} />
+            </>
+          )}
+          {formStep === 2 && (
+            <>
+              <CadastroEnd />
+            </>
+          )}
+        </section>
       </section>
       {formStep === 0 && (
         <Image
@@ -122,7 +124,7 @@ export default function Welcome({ user }) {
           height={1024}
         />
       )}
-    </section>
+    </>
   )
 }
 
