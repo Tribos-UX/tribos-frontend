21f35126bcbb7dd9fbb9e39bb67d3690c0397b0a
liafer <106263459+liafer@users.noreply.github.com>
2023-01-19 11:34:46 -0500

    add link group form

--- tribos-ux/components/Forms/NewGroup/GroupFormEnd.tsx
+++ tribos-ux/components/Forms/NewGroup/GroupFormEnd.tsx
@@ -1,10 +1,17 @@
 import styles from './styles/CadastroFormEnd.module.scss'
 
+import Link from 'next/link'
+
 export default function GroupFormEnd() {
   return (
-    <article className={styles.cadastro_end}>
-      Parabéns, seu grupo está pronto! <br></br> Você poderá alterar essas
-      informações quando quiser, dentro so perfil do seu grupo.
-    </article>
+    <>
+      <article className={styles.cadastro_end}>
+        Parabéns, seu grupo está pronto! <br></br> Você poderá alterar essas
+        informações quando quiser, dentro so perfil do seu grupo.
+      </article>
+      <div>
+        <Link href="/dashboard/home">Concluir</Link>
+      </div>
+    </>
   )
 }
