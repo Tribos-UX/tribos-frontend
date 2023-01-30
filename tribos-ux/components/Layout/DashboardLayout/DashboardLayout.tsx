1d48a777b32294fb4699e6545a6f6e68fb2af05d
viniciusmbc <viniciusmbc>
2023-01-30 20:04:32 -0300

    feat: add app bar

--- tribos-ux/components/Layout/DashboardLayout/DashboardLayout.tsx
+++ tribos-ux/components/Layout/DashboardLayout/DashboardLayout.tsx
@@ -4,10 +4,13 @@ import Sidebar from '../Sidebar/Sidebar'
 // Styles
 import styles from '../DashboardLayout/DashboardLayout.module.scss'
 
+import ButtonAppBar from '../AppBar/AppBar'
+
 export default function DashboardLayout({ children }) {
   return (
     <main className={styles.container}>
       <Sidebar />
+      <ButtonAppBar />
       {children}
     </main>
   )
